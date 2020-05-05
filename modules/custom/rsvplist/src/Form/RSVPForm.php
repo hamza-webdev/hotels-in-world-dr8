8cee8132e6e614c5daa7d197ff8f289b4339f55d
hamza-webhelpmedica <hamza.bedoui@webhelpmedica.com>
2020-05-06 00:14:22 +0200

    create rsvp form module

--- modules/custom/rsvplist/src/Form/RSVPForm.php
+++ modules/custom/rsvplist/src/Form/RSVPForm.php
@@ -18,7 +18,7 @@ use Drupal\Core\Form\FormBase;
 class RSVPForm extends FormBase {
   /**
    * (@inheritDoc)
-   * @return string
+   *
    */
   public function getFormId()
   {
@@ -31,13 +31,17 @@ class RSVPForm extends FormBase {
   public function buildForm(array $form, FormStateInterface $form_state)
   {
     $node = \Drupal::routeMatch()->getParameter('node');
-    $nid = $node->nid->value;
+
+    $nid = null;
+    if ($node instanceof \Drupal\node\NodeInterface) {
+      $nid = $node->id()->value;
+    }
     $form['email'] = array(
-      '@title' => t('Email address'),
-      '@type' => 'textfield',
-      '@size' => 25,
-      '@description' => t("We ' ll send update to the mail address you provide."),
-      '@required' => TRUE,
+      '#title' => t('Email address'),
+      '#type' => 'textfield',
+      '#size' => 25,
+      '#description' => t("We ' ll send update to the mail address you provide."),
+      '#required' => TRUE,
     );
     $form['submit'] = array(
       '#type' => 'submit',
@@ -48,6 +52,37 @@ class RSVPForm extends FormBase {
       '#value' => $nid,
     );
     return $form;
+  }
+
+  /**
+   * (@inheritDoc)
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state)
+  {
+    $value = $form_state->getValue('email');
+    if ($value == !\Drupal::service('email.validator')->isValid($value)) {
+      $form_state->setErrorByName('email', t('The email address %mail is not valid', array(
+        '%mail' => $value
+      )));
+    }
+  }
+
+
+  /**
+   * (@inheritDoc)
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state)
+  {
+    $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+    db_insert('rsvplist')
+      ->fields(array(
+        'mail' => $form_state->getValue('email'),
+        'nid' => $form_state->getValue('nid'),
+        'uid' => $user->id(),
+        'created' => time()
+      ))
+      ->execute();
+    drupal_set_message(t('Meri pour votre RSVP, vous etes dans la list des evenements '));
   }
 
 }
